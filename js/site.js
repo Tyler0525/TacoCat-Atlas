5f29ea1313da501c3d3d01a59cb628ac00593f8b
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-19 15:10:16 -0800

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,13 +1,56 @@
-// display amesage to the user
-function getMessage() {
+// pull the user entered from the page 
+function getValues() {
+    let userString = document.getElementById("checkPalindrome").value;
 
-    let userMessage = document.getElementById("message").value;
+    //Call reverse a String
+    let isPalindrome = checkPalindrome2(userString);
+
+    //Display the reverse String 
+    displayData(isPalindrome, userString);
+
+
+}
+
+//this is our generate data
+
+function checkPalindrome2(userString) {
+    userString = userString.toLowerCase();
+    let regex = /[a-z0-9]/gi;
+    userString = userString.replace(regex, "")
+    let revString = "";
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+
+    }
+    if (revString == userString) {
+        return true;
+    }
+    return false;
+}
+
+
+
+//Display out Reversed String to the Page 
+
+
+function displayData(isPalindrome, userString) {
+    if (isPalindrome == true) {
+
+        document.getElementById("alert").classList.remove("alert-danger");
+        document.getElementById("alert").classList.remove("alert-success");
+
+        
+        document.getElementById("alert").classList.add("alert-success");
+        document.getElementById("msg").innerHTML = `It is a Palindrome!; ${userString}`;
+        document.getElementById("alertheading").innerHTML = `You Did It!`;
+    } else {
+        document.getElementById("alert").classList.add("alert-danger");
+        document.getElementById("msg").innerHTML = `It is not a Palindrome...: ${userString}`;
+        document.getElementById("alertheading").innerHTML = `Sorry!`;
+    }
+
+
     
-Swal.fire({
-    icon: 'success',
-    title: 'THE APP SAYS',
-    userMessage,
-})
-
-    console.log("Hello Console Log");
+    document.getElementById("alert").classList.remove('invisible');
+    document.getElementById("alert").classList.remove('d-none');
 }
